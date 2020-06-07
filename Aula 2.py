177fb5f713eec0c6603e43ea01c9414c05cbcd2d
Mateus Porto <mateusportoalexandre96@hotmail.com>
2020-06-07 18:10:58 -0300

    Projetos python

---
+++ Aula 2.py
@@ -0,0 +1,12 @@
+a = int(input("Entre com o primeiro valor: "))
+b = int(input("Entre com o segundo valor: "))
+
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+print("SOMA:", soma )
+
+# x = "1"
+# soma2 = int(x) +1
+# print(soma2)
